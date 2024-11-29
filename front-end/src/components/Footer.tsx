ae01820c793db1b2445ed9d40cc7952abb18b1a1
phlucasfr <phlucasfr@gmail.com>
2024-11-29 18:00:26 -0300

    updated footer.

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer class="bg-blue-600/70 backdrop-blur-md text-white/80 p-4 fixed bottom-0 left-0 right-0 shadow-lg">
       <div class="container mx-auto text-center text-sm">
-        <p>&copy; 2024 <span class="font-semibold">Go Sales</span>. Todos os direitos reservados.</p>
+        <p>&copy; 2024 <span class="font-semibold">GoBizz</span>. Todos os direitos reservados.</p>
       </div>
     </footer>
   );
