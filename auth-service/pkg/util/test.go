96a40b9fdeae26b961b227c3e1bfb7ada0a94ffd
phlucasfr <phlucasfr@gmail.com>
2025-04-13 17:10:05 -0300

    (fix) removed old version

--- auth-service/pkg/util/test.go
+++
@@ -1,7 +0,0 @@
-package util
-
-import "os"
-
-func IsTesting() bool {
-	return os.Getenv("TEST_ENV") == "true"
-}
--- front-end/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_AUTH_SERVICE_API: string
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
