e85db722d71b94d7783ecd444ce35c5e8fd71391
Phelipe <phlucasfr@gmail.com>
2025-09-01 15:10:39 -0300

    fix(recurring-service) build

--- recurring-service/build.rs
+++ recurring-service/build.rs
@@ -1,5 +1,12 @@
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/events.proto")?;
+    unsafe { std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path()?) };
+
+    let wkt_include = protoc_bin_vendored::include_path().unwrap();
+
+    tonic_prost_build::configure().compile_protos(
+        &["proto/events.proto"],
+        &["proto", wkt_include.to_str().unwrap()],
+    )?;
+
     Ok(())
 }
